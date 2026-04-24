8947e03c30cd5b7357f37886e284d39a607567ec
M-Sarim10 <sarim10106@gmail.com>
2026-04-24 20:58:05 +0500

    Completed this class

--- Account.cpp
+++ Account.cpp
@@ -14,16 +14,37 @@
     void Account::deposit(double amount){
         if(amount>0){
           balance=balance+amount;
+          addTransaction(new Transaction("Deposit", amount));
+          saveToFile();
         }
     }
     bool Account::withdraw(double amount){
         if(amount<=balance){
             balance=balance-amount;
+            addTransaction(new Transaction("Withdraw", -amount));
+            saveToFile();
             return true;
         }
         return false;
     }
-    bool transfer(Account* target, double amount);
+    bool Account::transfer(Account* target, double amount){
+        if(amount<=0||amount>balance){
+            cout<<"Invalid amount cant work on it"<<endl;
+            return false;
+        }
+        else if(accountNumber==target->accountNumber){
+            cout<<"Invalid number cant transfer"<<endl;
+            return false;
+        }
+        balance=balance-amount;
+        target->balance=target->balance+amount;
+        addTransaction(new Transaction("Deducted",-amount));
+        target->addTransaction(new Transaction("Received",amount));
+        saveToFile();
+        target->saveToFile();
+        cout<<"Transaction Successful and money Transferred"<<endl;
+        return true;
+    }
     double Account::getBalance(){
          return balance;
     }
@@ -34,7 +55,10 @@
         }
     }
     }
-    void addTransaction(Transaction* t);
+    void Account::addTransaction(Transaction* t){
+    *(transactions + transactionCount) = t;
+    transactionCount++;
+    }
  void Account::saveToFile() {
     ifstream infile("account_record.txt");
     ofstream temp_f("temp.txt");
@@ -90,13 +114,14 @@
                 break;
             }
             if(id==accountNumber){
+                int tempcount;
                 infile>>balance;
-                infile>>transactionCount;
-                for (int i = 0; i < transactionCount; i++) {
+                infile>>tempcount;
+                transactionCount=0;
+                for (int i = 0; i < tempcount; i++) {
                 float amt;
                 infile>>amt;
-                Transaction* t = new Transaction("History", amt);
-                *(transactions + i) = t;
+                addTransaction(new Transaction("History", amt));
             }
             break;
         }
