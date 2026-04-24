3bc6a9a4944e90cc01bccacea5c2c48a0beb11bd
Abdullah Asif <abdullahasifn18@gmail.com>
2026-04-24 18:46:47 +0500

    update: added admin class

--- main.cpp
+++ main.cpp
@@ -1,96 +1,152 @@
-#include<iostream>
-#include<fstream>
-#include<time.h>
-#include<cstring>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <cstring>
+using namespace std;
 
-class User {
+class User//abstract base, meaning no direct obj creation
+{
 protected:
-    string name, accountNumber, pin;
+    string name;
+    string accountNumber;
+    string pin;
+
 public:
     User(string n, string acc, string p);
-    bool verifyPin(string input);   
+    virtual bool verifyPin(string input) = 0; // pure virtual meaning abstraction
+    virtual void showMenu() = 0;              // pure virtual — each user type has different menu
+    virtual string getRole() = 0;             // pure virtual — returns "RegisteredUser" or "Admin"
     string getName();
+    string getAccountNumber();
+    virtual ~User();
 };
-//------------------------------------------------------------
-class RegisteredUser : public User {
+//--------------------------------------------------------------------------------------
+class RegisteredUser : public User
+{
 private:
     int failedAttempts;
     bool isLocked;
-    Account* account;//composition
+    Account *account; // composition
 public:
-    RegisteredUser(string n, string acc, string p, Account* a);
-    bool verifyPin(string input);   
+    RegisteredUser(string n, string acc, string p, Account *a);
+    bool verifyPin(string input) override;
+    void showMenu() override;
+    string getRole() override;
     void lockAccount();
     void changePin(string oldPin, string newPin);
-    Account* getAccount();
+    Account *getAccount();
     bool getIsLocked();
+    ~RegisteredUser();
 };
-//---------------------------------------------------------------
-class Account {
+//----------------------------------------------------------------------------------
+class Admin : public User
+{
+private:
+    double atmCashAvailable; 
+public:
+    Admin(string n, string acc, string p);
+    bool verifyPin(string input) override;
+    void showMenu() override;
+    string getRole() override;
+    void refillATMCash(double amount);              
+    void addAccount(Account *acc);                  
+    void removeAccount(string accNo);               
+    void viewAllTransactions();                     
+    void resetUserPin(string accNo, string newPin); 
+    double getATMCash();
+    ~Admin();
+};
+//-------------------------------------------------------------------------------------------
+class Account
+{
 private:
     string accountNumber;
+    string holderName;
     double balance;
-    Transaction* transactions[50];  // array of pointers meaning aggregation
+    bool isActive;
+    Transaction *transactions[50]; // aggregation
     int transactionCount;
+
 public:
-    Account(string accNo, double initialBalance);
+    Account(string accNo, string holderName, double initialBalance);
     ~Account();
     void deposit(double amount);
     bool withdraw(double amount);
-    bool transfer(Account* target, double amount);
+    bool transfer(Account *target, double amount);
     double getBalance();
+    string getAccountNumber();
+    string getHolderName();
+    bool getIsActive();
+    void setIsActive(bool status);
     void printMiniStatement();
-    void addTransaction(Transaction* t);
+    void addTransaction(Transaction *t);
     void saveToFile();
     void loadFromFile();
+    void showBalance();  
 };
-//---------------------------------------------------------------
-class Transaction {
+//---------------------------------------------------------------------------------
+class Transaction//abstract base, cant make a transaction obj directly
+{
 protected:
-    string type, timestamp;
+    string type;
+    string timestamp;
     double amount;
+
 public:
     Transaction(string t, double amt);
-    void execute(Account* acc);     
-    void printReceipt();            
+    virtual void execute(Account *acc) = 0; // pure virtual
+    virtual void printReceipt() = 0;        // pure virtual
     double getAmount();
+    string getType();
+    string getTimestamp();
+    virtual ~Transaction(); 
 };
-//-------------------------------------------------------------
-class Withdraw : public Transaction {
+//-----------------------------------------------------------------------------
+class Withdraw : public Transaction
+{
 private:
     double limit;
+
 public:
-    Withdraw(double amt);
-    void execute(Account* acc);     
-    void printReceipt();            
+    Withdraw(double amt, double limit = 25000);
+    void execute(Account *acc) override;
+    void printReceipt() override;
+    ~Withdraw();
 };
-//-------------------------------------------------------------
-class Deposit : public Transaction {
+//---------------------------------------------------------------------------------
+class Deposit : public Transaction
+{
 private:
     string source;
+
 public:
     Deposit(double amount, string source = "Cash");
-    void execute(Account* acc);
-    void printReceipt();
+    void execute(Account *acc) override;
+    void printReceipt() override;
+    ~Deposit();
 };
-//-------------------------------------------------------------
-class Transfer : public Transaction {
+//----------------------------------------------------------------------------
+class Transfer : public Transaction
+{
 private:
     string toAccountNumber;
-    Account* targetAccount;
+    Account *targetAccount; // aggregation
 public:
-    Transfer(double amount, string toAccNo, Account* target);
-    void execute(Account* acc);
-    void printReceipt();
+    Transfer(double amount, string toAccNo, Account *target);
+    void execute(Account *acc) override;
+    void printReceipt() override;
+    ~Transfer();
 };
-//-------------------------------------------------------------
-//-------------------------------------------------------------
-class ATM {
+//---------------------------------------------------------------------------------
+class ATM
+{
 private:
-    User* currentUser;//aggregation
-    Account* accounts[100];//aggregation
+    User *currentUser;      // aggregation 
+    Account *accounts[100]; // aggregation
     int accountCount;
+    double cashAvailable;
     ofstream logFile;
+
 public:
     ATM();
     ~ATM();
@@ -100,5 +156,7 @@ public:
     void processTransaction(int choice);
     void loadAccounts();
     void saveAccounts();
-    void logTransaction(Transaction* t);
+    void logTransaction(Transaction *t);
+    void refillCash(double amount); // called by Admin
+    double getCashAvailable();
 };
