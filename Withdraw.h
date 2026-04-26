d0086d97832cb84044cca2d868d484b75c3ef5ab
zoha fatima <aleeha.zar@gmail.com>
2026-04-26 22:34:46 +0500

    Create Withdraw.h

---
+++ Withdraw.h
@@ -0,0 +1,17 @@
+#ifndef WITHDRAW_H
+#define WITHDRAW_H
+#include"Transaction.h"
+#include"Account.h"
+#include <string>
+class Withdraw :public Transaction
+{
+private:
+	double limit;
+public:
+	Withdraw(double amt, double limitValue = 25000.0);//amt is desired amount user wanna take out
+	void execute(Account* acc) override;
+	void printReceipt() override;
+	~Withdraw();
+
+};
+#endif
